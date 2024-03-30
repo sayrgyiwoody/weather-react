a84779813c3c2b5d35e3305f768b590a75d8f4e3
Woody <waiyanwoody@gmail.com>
2024-03-30 23:12:06 +0630

    cloud icon fixed

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const WeatherInfo = ({data}) => {
+
+
     
     const kelvinToFahrenheit = (kelvinValue) => {
         const fahrenheit = (( kelvinValue - 273.15) * 9/5) + 32;
@@ -13,11 +15,12 @@ const WeatherInfo = ({data}) => {
     }
 
     if(JSON.stringify(data) !== '{}'){
+
         return (
             <div className='text-center mt-6'>
               <h2 className=' text-2xl font-medium '>{data.name} , {data.sys.country}</h2>
-              <p className=' text-gray-700'>few clouds</p>
-              <img className=' mx-auto' src={`http://openweathermap.org/img/w/02n.png`} alt="weather icon" />
+              <p className=' text-gray-700'>{data.weather[0].description}</p>
+              <img className=' mx-auto' src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} alt="weather icon" />
         
               <h2 className=' my-4 text-4xl font-medium '>{kelvinToCelsius(data.main.temp)} °C</h2>
               <p className=' text-gray-700'>temperature from <span className=' font-medium '>{kelvinToCelsius(data.main.temp_min)}</span> to <span className=' font-medium '>{kelvinToCelsius(data.main.temp_max)}°C</span>,</p>
